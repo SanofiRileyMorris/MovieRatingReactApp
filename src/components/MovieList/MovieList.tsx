631496e6c3effaa6de6dc55ab3a1afd239ee6ba1
Andre Gomes <andre.gomes@sanofi.com>
2024-08-19 15:46:19 +0200

    feat: skeleton for movielist

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,99 +1,64 @@
 import { Box, Container } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { RedButton } from "../StyledMUI/StyledMUI";
 import styles from "./MovieList.module.css";
 
-export const MovieList = (movie: any) => {
-    const movieTitle: string = movie.movie;
-    const apiUrl: string = `https://www.omdbapi.com/?t=${encodeURIComponent(movieTitle)}&apikey=${process.env.REACT_APP_API_KEY}`;
-    // const apiUrl: string = ` http://www.omdbapi.com/?apikey=${apiKey}&`;
+type MovieApi = {
+    title: string;
+    id: string;
+    poster_path: string;
+}
 
-
-    const [movieData, setMovieData] = useState<MovieData>();
-
-    interface MovieData {
-        Title: string;
-        Year: string;
-        Rated: string;
-        Released: string;
-        Runtime: string;
-        Genre: string;
-        Director: string;
-        Writer: string;
-        Actors: string;
-        Plot: string;
-        Language: string;
-        Country: string;
-        Awards: string;
-        Poster: string;
-        Ratings: Array<{ Source: string; Value: string }>;
-        Metascore: string;
-        imdbRating: string;
-        imdbVotes: string;
-        imdbID: string;
-        Type: string;
-        DVD: string;
-        BoxOffice: string;
-        Production: string;
-        Website: string;
-        Response: string;
-    }
-
-    async function fetchMovieData(url: string): Promise<void> {
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            const data: MovieData = await response.json();
-            setMovieData(data)
-            console.log(data);
-        } catch (error) {
-            console.error('Error fetching movie data:', error);
+async function getTopRatedMovies(): Promise<MovieApi[]> {
+    return fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200`, {
+        headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+            accept: "application/json"
         }
+    })
+        .then(result => result.json())
+        .then(data => data.results as MovieApi[]);
+}
+
+
+export const MovieList = () => {
+    const [movies, setMovieData] = useState<MovieApi[] | null>();
+    const [loadingState, setLoadingState] = useState(false);
+    const [isError, setIsError] = useState(false);
+
+
+    useEffect(() => {
+        setLoadingState(true);
+        getTopRatedMovies().then((movies) => {
+            setMovieData(movies);
+            setLoadingState(false);
+        })
+        .catch(() => {
+            setIsError(true);
+        });
+    }, [])
+
+    if(loadingState) {
+        return <div>Loading...</div>
     }
 
+    if(isError) {
+        return <div>Something went wrong</div>
+    }
 
-    const handleSearch = async () => {
-        fetchMovieData(apiUrl);
-    };
+    if(!movies) {
+        return null;
+    }
 
-
+    // paginate would be nice 
     return (
         <div>
-
-            <div className={styles.SearchButton}>
-                <RedButton onClick={handleSearch}> Search</RedButton>
-            </div>
-            {movieData !== undefined && (
-                <Container maxWidth="sm">
-                    <Box sx={{ bgcolor: 'white', padding: "3rem", borderRadius: "2rem" }}>
-                        {/* <span>
-                            Title: {movieData?.Title}
-                        </span> */}
-                        <div>
-                            {movieData && typeof movieData === 'object' && (
-                                <div>
-                                    {movieData && typeof movieData === 'object' && (
-                                        <div>
-                                            {Object.entries(movieData).map(([meta, data]) => (
-                                                <div className={styles.movieItems}>
-                                                    <h4 className={styles.meta}>
-                                                        {meta}: {JSON.stringify(data)}
-                                                    </h4>
-                                                </div>
-
-
-                                            ))}
-                                        </div>
-                                    )}
-
-                                </div>
-                            )}
-                        </div>
-                    </Box>
-                </Container>
-            )}
+            {movies.map((movie) => (
+                <div key={movie.id} onClick={() => navigate(`/movie/${id}`)}>
+                    <h1>{movie.title}</h1>
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                </div>
+            ))}
         </div>
     );
 };
